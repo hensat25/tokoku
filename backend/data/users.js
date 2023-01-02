01c235734f344dbbfa853d7d25fc5b6ccc3a10ff
eennugraha <nugraha.hendrisatya@gmail.com>
2023-01-02 16:20:35 +0700

    added controllers for user & product

--- backend/data/users.js
+++ backend/data/users.js
@@ -11,13 +11,11 @@ const users = [
     name: 'John Doe',
     email: 'john@example.com',
     password: bcrypt.hashSync('123456', 10),
-    isAdmin: true,
   },
   {
     name: 'Jane Doe',
     email: 'jane@example.com',
     password: bcrypt.hashSync('123456', 10),
-    isAdmin: true,
   },
 ];
 
