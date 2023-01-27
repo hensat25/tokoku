3e050c3f3008d2983f530186fcef1096e22950b8
eennugraha <nugraha.hendrisatya@gmail.com>
2023-01-27 16:21:41 +0700

    added order details, order actions and order screen. added some changes to order model

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -26,11 +26,20 @@ const orderSchema = mongoose.Schema(
       postalCode: { type: String, required: true },
       country: { type: String, required: true },
     },
+    paymentMethod: {
+      type: String,
+      required: true,
+    },
     paymentResult: {
       id: { type: String },
       status: { type: String },
       update_time: { type: String },
       email_address: { type: String },
+    },
+    itemsPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
     },
     taxPrice: {
       type: Number,
