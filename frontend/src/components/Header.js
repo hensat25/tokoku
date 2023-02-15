7982f7a65b6185c6aac0fe336d806e460f9cb0b8
eennugraha <nugraha.hendrisatya@gmail.com>
2023-02-15 17:14:01 +0700

    fixed not rredirecting after logout

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import { logout } from '../actions/userActions';
 
 const Header = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
 
   const logoutHandler = () => {
     dispatch(logout());
+    navigate('/login');
   };
 
   return (
