d45df0aacfbefaba9faf8177325f85456c592b34
hensat25 <nugraha.hendrisatya@gmail.com>
2023-03-17 21:16:24 +0700

    adjusting searchbox layout

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -17,15 +17,15 @@ const SearchBox = () => {
   };
 
   return (
-    <Form onSubmit={submitHandler} inline>
+    <Form onSubmit={submitHandler} className="d-flex">
       <Form.Control
         type="text"
         name="q"
         onChange={(e) => setKeyword(e.target.value)}
         placeholder="Search Products..."
-        className="mr-sm-2 ml-sm-5"
       ></Form.Control>
-      <Button type="submit" variant="outline-success" className="p-2">
+
+      <Button type="submit" variant="outline-success">
         Search
       </Button>
     </Form>
